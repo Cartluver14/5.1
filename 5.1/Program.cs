dd5c6de9af9ae79ea5051c23d186533e14188080
North Harrington <339713067@GoTVDSB.ca>
2024-10-01 08:57:56 -0400

    done

--- 5.1/Program.cs
+++ 5.1/Program.cs
@@ -4,61 +4,33 @@
     {
         static void Main(string[] args)
         {
-            double earthweight;
-            int planet;
+            int number;
+            int rightnumber;
 
-            Console.WriteLine("What planet would you like me to fight on");
-            Console.WriteLine("1. Venus 2. Mars 3. Jupiter 4. Saturn 5. Uranus 6. Neptune");
-            planet = int.Parse(Console.ReadLine());
-            Console.WriteLine(" What is your weight on earth?");
-            earthweight = double.Parse(Console.ReadLine());
+            rightnumber = 7;
 
-            if (planet == 1)
+            Console.WriteLine("guess a number from 1 to 10 to win!");
+            int.TryParse(Console.ReadLine(), out number);
+
+            if (number == 7)
             {
-                Console.WriteLine(" on venus your weight would be " + earthweight * 0.78 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
 
-            }
-
-            if (planet == 2)
-            {
-                Console.WriteLine(" on mars your weight would be " + earthweight * 0.39 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
+                Console.WriteLine("Congrats you won!");
 
 
-            }
-
-            if (planet == 3)
-            {
-                Console.WriteLine(" on jupiter your weight would be " + earthweight * 2.65 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
 
 
-            }
-
-            if (planet == 4)
-            {
-                Console.WriteLine(" on saturn your weight would be " + earthweight * 1.17 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
-
-
-            }
-
-            if (planet == 5)
-            {
-                Console.WriteLine(" on urainus your weight would be " + earthweight * 1.05 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
 
 
 
             }
-
-            if (planet == 6)
+            else
             {
-                Console.WriteLine(" on venus your weight would be " + earthweight * 1.23 + "Lbs");
-                Console.WriteLine(" get that dub sigma");
+                Console.WriteLine("Your wrong its acutully " +  rightnumber);
 
 
             }
-        } } }
-
+               
+        }
+    }
+}
